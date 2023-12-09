68f55212c8eb007f00b83b707689ca523b2f55ed
MertKayabasi <139959091+MertKayabasi@users.noreply.github.com>
2023-12-09 18:23:17 +0300

    Update Main.java

--- BİL429_PROJE/src/portScanner/Main.java
+++ BİL429_PROJE/src/portScanner/Main.java
@@ -1,10 +1,13 @@
 package portScanner;
 import javax.swing.SwingUtilities;
 public class Main {
-    public static void main(String[] args) {
-    	  SwingUtilities.invokeLater(() -> {
-    	        PortScannerGUI app = new PortScannerGUI();
-    	        app.setVisible(true);
-    	    });
-    }
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                PortScannerGUI portScannerGUI = new PortScannerGUI();
+                portScannerGUI.setVisible(true);
+            }
+        });
+	}
 }
